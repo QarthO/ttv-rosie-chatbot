510723e898303b8f45c61ed0708fc014b346345b
QarthO <QarthO@gmail.com>
2021-02-02 02:33:39 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -11,62 +11,65 @@ import datetime
 from random import randint
 import requests
 
-irc_token = ''
-client_id = ''
-bot_nick = ''
-command_prefix = '!'
-channel = ''
+from commands import Commands
 
-cmds = ''
-broadcasts = ''
-listeners = ''
+#Reads auth data
+def read_auth():
+    with open('config/auth.yml') as f_auth:
+        data = yaml.load(f_auth, Loader=yaml.FullLoader)
+        bot_username = data.get('bot-name')
+        irc_token = data.get('auth-id')
+        client_id = data.get('client-id')
+        cmd_prefix = data.get('cmd-prefix')
+        channel = data.get('channel')
+    return (bot_username, irc_token, client_id, cmd_prefix, channel)
 
-# Reads config files
-with open('config/auth.yml') as f_auth, open('config/commands.yml') as f_commands, open('config/broadcasts.yml') as f_broadcasts, open('config/listeners.yml') as f_listeners:
-    data = yaml.load(f_auth, Loader=yaml.FullLoader)
-    irc_token = data.get('auth-id')
-    client_id = data.get('client-id')
-    bot_nick = data.get('bot-name')
-    command_prefix = data.get('cmd-prefix')
-    channel = data.get('channel')
-    data = yaml.load(f_commands, Loader=yaml.FullLoader)
-    cmds = data.get('commands')
-    data = yaml.load(f_broadcasts, Loader=yaml.FullLoader)
-    broadcasts = data.get('broadcasts')
-    data = yaml.load(f_listeners, Loader=yaml.FullLoader)
-    listeners = data.get('listeners')
+#Reads config
+def read_config():
+    with open('config/broadcasts.yml') as f_broadcasts:
+        data = yaml.load(f_broadcasts, Loader=yaml.FullLoader)
+        broadcasts = data.get('broadcasts')
+    return broadcasts
+
+config_data = read_config() #config_data = broadcasts
+broadcasts = config_data
 
 # Main Twitch Bot Class
 class Bot(commands.Bot):
-
     cmds_on_cooldown = dict()
     n_current = 0
-    time_last_broadcast = ''
-
     def __init__(self):
+        self.auth_data = read_auth() #auth_data = (bot_username, irc_token, client_id, cmd_prefix, channel)
+        self.bot_username = self.auth_data[0]
+        self.irc_token = self.auth_data[1]
+        self.client_id = self.auth_data[2]
+        self.cmd_prefix = self.auth_data[3]
+        self.channel = self.auth_data[4]
+
         super().__init__(
-            irc_token=irc_token,
-            client_id=client_id,
-            nick=bot_nick,
-            prefix=command_prefix,
-            initial_channels=[channel]
+            nick = self.bot_username,
+            irc_token = self.irc_token,
+            client_id = self.client_id,
+            prefix = self.cmd_prefix,
+            initial_channels = [self.channel]
         )
+
+        self.bot_commands = Commands('config/commands.yml', self.cmd_prefix)
 
     # On bot startup
     async def event_ready(self):
-        print(f'\nBot: {bot_nick}')
-        print(f'Channel: {channel}')
+        print(f'\nBot: {self.bot_username}')
+        print(f'Channel: {self.channel}')
         print('Connected!\n')
         ws = bot._ws
-        await ws.send_privmsg(channel, f"/me is online")
+        await ws.send_privmsg(self.channel, f"/me is online")
 
     # Everytime a message is sent in the channel
     async def event_message(self, message):
-        if message.author.name == 'roseiol': return
+        if message.author.name == self.bot_username: return # ignores itself
+        await self.bot_commands.handler(message, await self.get_context(message))
         await self.handle_commands(message)
-        await self.handle_listener(message)
         await self.handle_broadcast(message)
-
 
 
 #  ██████╗ ██████╗ ███╗   ███╗███╗   ███╗ █████╗ ███╗   ██╗██████╗     ██╗  ██╗ █████╗ ███╗   ██╗██████╗ ██╗     ███████╗██████╗
@@ -76,11 +79,12 @@ class Bot(commands.Bot):
 # ╚██████╗╚██████╔╝██║ ╚═╝ ██║██║ ╚═╝ ██║██║  ██║██║ ╚████║██████╔╝    ██║  ██║██║  ██║██║ ╚████║██████╔╝███████╗███████╗██║  ██║
 #  ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝
 
-    @commands.command(name='commands', aliases={'cmds'})
-    async def cmd_list_commands(self, ctx):
-        cmd_label_list = cmds.keys()
-        response = '/me !' + ', !'.join(cmd_label_list + ', !shoutout, !so, !timeout, !vanish')
-        await ctx.send(response)
+    # @commands.command(name='commands', aliases={'cmds'})
+    # async def cmd_list_commands(self, ctx):
+    #     cmd_label_list = simple_cmds.keys()
+    #     cmd_list = ', !'.join(cmd_label_list) + ', !shoutout, !so, !timeout, !vanish'
+    #     response = '/me !' + cmd_list
+    #     await ctx.send(response)
 
     @commands.command(name='shoutout', aliases={'so'})
     async def cmd_shoutout(self, ctx):
@@ -95,64 +99,6 @@ class Bot(commands.Bot):
         response = f'/me BOOBA go --> https://www.twitch.tv/{who} <-- or i ziggs ult you . . . (in game)'
         await ctx.send(response)
 
-    @commands.command(name='timeout')
-    async def cmd_timeout(self, ctx):
-        username = ctx.author.name
-        time = 5
-        reason = 'u timed urself out'
-        print(f'Self timing out: {username}')
-        await ctx.send(f'/me bye bye @{username}!!!')
-        await ctx.timeout(user=username, duration=time, reason=reason)
-
-    @commands.command(name='vanish')
-    async def cmd_vanish(self, ctx):
-        username = ctx.author.name
-        time = 1
-        reason = 'Clearing your chat messages'
-        print(f'Vanishing: {username}')
-        await ctx.timeout(user=username, duration=time, reason=reason)
-
-    @commands.command(name='commands_from_file', aliases=cmds.keys())
-    async def commands_from_file(self, ctx):
-        cmd_label = ctx.content[1:]
-        cmd = cmds.get(cmd_label)
-        if not self.check_roles(cmd_label, ctx.author): return
-        if not self.check_cooldown(cmd_label):
-            response = cmd.get('response')
-            cooldown = cmd.get('cooldown')
-            available_time = datetime.datetime.now() + datetime.timedelta(seconds=cooldown)
-            self.cmds_on_cooldown[cmd_label] = available_time
-            await ctx.send(f'/me {response}')
-        self.check_roles(cmd_label, ctx.author)
-
-    @commands.command(name='title')
-    async def cmd_title(self, ctx):
-        msg = ctx.content
-        user_roles = self.get_roles(ctx.author)
-        cmd_roles = ['mod', 'broadcaster']
-        mixed = set(cmd_roles) & set(user_roles)
-        if not mixed: return
-        if len(msg) < 2: return
-        new_title = msg.split(" ")[1:]
-        self.change_title(new_title)
-        await ctx.send(f'/me title changed to: {new_title}')
-
-    def check_cooldown(self, cmd_label):
-        if cmd_label in self.cmds_on_cooldown.keys():
-            available_time = self.cmds_on_cooldown.get(cmd_label)
-            if datetime.datetime.now() > available_time:
-                del self.cmds_on_cooldown[cmd_label]
-                return False
-            return True
-        return False
-
-    def check_roles(self, cmd_label, user):
-        cmd = cmds.get(cmd_label)
-        cmd_roles = cmd.get('roles')
-        user_roles = self.get_roles(user)
-        mixed = set(cmd_roles) & set(self.get_roles(user))
-        return True if mixed else False
-
     def get_roles(self, user):
         roles = []
         badges = user._badges
@@ -163,12 +109,22 @@ class Bot(commands.Bot):
         roles.append('pleb')
         return roles
 
-    def change_title(self, title):
-        url = "https://api.twitch.tv/kraken/channels/roselol"
-        headers = {"Client-ID" : ""+ client_id +"", "Authorization": "OAuth "+ "oauth:spuet8fibbw5kczk9pmu2xkb34j54n"}
-        data = {"channel": {"status": title}}
-        response = requests.put(url=url, headers=headers, params = data)
-        print(response.status_code)
+    # @commands.command(name='timeout')
+    # async def cmd_timeout(self, ctx):
+    #     username = ctx.author.name
+    #     time = 5
+    #     reason = 'u timed urself out'
+    #     print(f'Self timing out: {username}')
+    #     await ctx.send(f'/me bye bye @{username}!!!')
+    #     await ctx.timeout(user=username, duration=time, reason=reason)
+
+    # @commands.command(name='vanish')
+    # async def cmd_vanish(self, ctx):
+    #     username = ctx.author.name
+    #     time = 1
+    #     reason = 'Clearing your chat messages'
+    #     print(f'Vanishing: {username}')
+    #     await ctx.timeout(user=username, duration=time, reason=reason)
 
 # ██████╗ ██████╗  ██████╗  █████╗ ██████╗  ██████╗ █████╗ ███████╗████████╗    ██╗  ██╗ █████╗ ███╗   ██╗██████╗ ██╗     ███████╗██████╗
 # ██╔══██╗██╔══██╗██╔═══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔════╝╚══██╔══╝    ██║  ██║██╔══██╗████╗  ██║██╔══██╗██║     ██╔════╝██╔══██╗
@@ -179,32 +135,14 @@ class Bot(commands.Bot):
 
     async def handle_broadcast(self, message):
         if message.author.name == 'roseiol': return
-        msg = message.content.split(" ")
-        if (set(msg) & set(listeners.keys())) or message.content[0] == "!": return
+        msg = message.content.split(" ") 
+        if message.content[0] == "!": return
         if randint(1,15) == 3:
             ws = bot._ws
-            await ws.send_privmsg(channel, f'/me {broadcasts[self.n_current]}')
+            await ws.send_privmsg(self.channel, f'/me {broadcasts[self.n_current]}')
             if self.n_current >= (len(broadcasts)-1): self.n_current = 0
             else: self.n_current +=1
         return
-
-# ██╗     ██╗███████╗████████╗███████╗███╗   ██╗███████╗██████╗     ██╗  ██╗ █████╗ ███╗   ██╗██████╗ ██╗     ███████╗██████╗
-# ██║     ██║██╔════╝╚══██╔══╝██╔════╝████╗  ██║██╔════╝██╔══██╗    ██║  ██║██╔══██╗████╗  ██║██╔══██╗██║     ██╔════╝██╔══██╗
-# ██║     ██║███████╗   ██║   █████╗  ██╔██╗ ██║█████╗  ██████╔╝    ███████║███████║██╔██╗ ██║██║  ██║██║     █████╗  ██████╔╝
-# ██║     ██║╚════██║   ██║   ██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗    ██╔══██║██╔══██║██║╚██╗██║██║  ██║██║     ██╔══╝  ██╔══██╗
-# ███████╗██║███████║   ██║   ███████╗██║ ╚████║███████╗██║  ██║    ██║  ██║██║  ██║██║ ╚████║██████╔╝███████╗███████╗██║  ██║
-# ╚══════╝╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝
-
-    async def handle_listener(self, message):
-        msg = message.content
-        for trigger in listeners.keys():
-            if str(trigger) in msg.lower().split(' '):
-                response = listeners.get(trigger)
-                ws = bot._ws
-                await ws.send_privmsg(channel, f'/me {response}')
-                return
-        return
-
 
 bot = Bot()
 bot.run()
